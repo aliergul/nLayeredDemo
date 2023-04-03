443a4f27d4a979725e203d0ada1bc2506a6da62a
aliergul <m.alieergul@gmail.com>
2023-04-04 00:59:09 +0300

    Sınıfların yazılması ve ufak bir test kodu.

--- nLayeredDemo/src/nLayeredDemo/Main.java
+++ nLayeredDemo/src/nLayeredDemo/Main.java
@@ -1,10 +1,29 @@
 package nLayeredDemo;
 
+import nLayeredDemo.business.abstracts.ProductService;
+import nLayeredDemo.business.concretes.ProductManager;
+import nLayeredDemo.dataAccess.concretes.AbcProductDao;
+import nLayeredDemo.dataAccess.concretes.HibernateProductDao;
+import nLayeredDemo.entities.concretes.Product;
+
 public class Main {
 
 	public static void main(String[] args) {
-		//hello github
 		
+		//ToDo: Spring ile çalışıldığı zaman buna gerek kalmayacak.
+		
+		ProductService productService1 = new ProductManager(new HibernateProductDao());
+		
+		Product product1 = new Product(1,2,"Çikolata",12,60);
+		productService1.add(product1);
+		
+		
+		ProductService productService2 = new ProductManager(new AbcProductDao());
+		
+		Product product2 = new Product(1,2,"Çikolata",12,60);
+		productService2.add(product2);
+		
+		////////////////////////////////////////////////////////////
 	}
 
 }
