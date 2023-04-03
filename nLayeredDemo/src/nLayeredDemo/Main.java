193e9edc2f439f6be2f837635a804223dddbbdaa
aliergul <m.alieergul@gmail.com>
2023-04-04 02:12:02 +0300

    Son commit: Core paketine dışarıdan sisteme entegre edilebilecek bir loglama simülasyonu eklendi

--- nLayeredDemo/src/nLayeredDemo/Main.java
+++ nLayeredDemo/src/nLayeredDemo/Main.java
@@ -2,6 +2,7 @@ package nLayeredDemo;
 
 import nLayeredDemo.business.abstracts.ProductService;
 import nLayeredDemo.business.concretes.ProductManager;
+import nLayeredDemo.core.jLoggerManagerAdapter;
 import nLayeredDemo.dataAccess.concretes.AbcProductDao;
 import nLayeredDemo.dataAccess.concretes.HibernateProductDao;
 import nLayeredDemo.entities.concretes.Product;
@@ -12,15 +13,15 @@ public class Main {
 		
 		//ToDo: Spring ile çalışıldığı zaman buna gerek kalmayacak.
 		
-		ProductService productService1 = new ProductManager(new HibernateProductDao());
+		ProductService productService1 = new ProductManager(new HibernateProductDao(), new jLoggerManagerAdapter());
 		
 		Product product1 = new Product(1,2,"Çikolata",12,60);
 		productService1.add(product1);
 		
 		
-		ProductService productService2 = new ProductManager(new AbcProductDao());
+		ProductService productService2 = new ProductManager(new AbcProductDao(), new jLoggerManagerAdapter());
 		
-		Product product2 = new Product(1,2,"Çikolata",12,60);
+		Product product2 = new Product(1,2,"Elma",7,32);
 		productService2.add(product2);
 		
 		////////////////////////////////////////////////////////////
